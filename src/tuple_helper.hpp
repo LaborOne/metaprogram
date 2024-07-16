acdfcd23a86b4dc94b24e25a6003710a845de0e8
wm <980577868@qq.com>
2024-07-16 21:17:55 +0800

    tuplecat

--- src/tuple_helper.hpp
+++ src/tuple_helper.hpp
@@ -13,10 +13,9 @@ template <class T> inline constexpr bool is_tuple_v = is_tuple<T>::value;
 template <class T> concept IsTuple = is_tuple_v<T>;
 
 namespace detail {
-template <IsTuple T, IsTuple... Args> struct tuple_cat_impl {
-  using type =
-      typename tuple_cat_impl<T, typename tuple_cat_impl<Args...>::type>::type;
-};
+template <IsTuple T, IsTuple... Args>
+struct tuple_cat_impl
+    : tuple_cat_impl<T, typename tuple_cat_impl<Args...>::type> {};
 template <class... lhs, class... rhs>
 struct tuple_cat_impl<std::tuple<lhs...>, std::tuple<rhs...>> {
   using type = std::tuple<lhs..., rhs...>;
